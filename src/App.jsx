85230d8a278d5e5322865429014d4fb4ba0f90d6
RobertVai <vaitechovskij@gmail.com>
2025-11-16 19:50:23 +0200

    added functions to add,delete,clear,filter tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,70 @@
 import { useState } from "react";
+
 import "./App.css";
 
 function App() {
+  const [task,setTask] = useState ("")
+  const [tasks,setTasks] = useState ([])
+  const [filter,setFilter] = useState ("all")
+  const [theme,setTheme] = useState ("light")
+
+  const addTask = () => {
+    if (task.trim() === "") return;
+    const newTask = {
+      id: Math.random(),
+      text: task,
+      completed: false
+    }
+    setTasks ([...tasks,newTask]);
+    setTask("");
+  }
+
+  const deleteTask = (id) => {
+    setTasks (tasks.filter((t) => t.id !== id))
+  }
+
+  const toggleTask = (id) => {
+    setTasks (tasks.map((t) => t.id === id ? {...t, completed: !t.completed} : t))
+  }
+
+  const filteredTasks = tasks.filter((t) =>{
+    if (filter === "active") return !t.completed;
+    if (filter === "done") return t.completed;
+    return true;
+  })
+
+  const clearTasks = () => {
+    setTasks([])
+  }
+  
   
 
   return (
     <div>
+      <input 
+      type="text"
+      placeholder="Add a new task ..."
+      value={task}
+      onChange={(e) => setTask(e.target.value)}
+      ></input>
+      <button onClick={addTask}>ADD</button>
 
+      {filteredTasks.map((t) => (
+        <li key={t.id}>
+          <input type="checkbox"
+          checked={t.completed}
+          onClick={() => toggleTask(t.id)}></input>
+          <p style={{textDecoration: t.completed ? "line-through" : "none"}}>{t.text}</p>
+          <button onClick={() => deleteTask(t.id)}>X</button>
+
+        </li>
+      ))}
+
+      <button onClick={() => setFilter("all")}>ALL</button>
+      <button onClick={() => setFilter("active")}>ACTIVE</button>
+      <button onClick={() => setFilter("done")}>DONE</button>
+
+      <button onClick={clearTasks}>CLEAR TASKS</button>
     </div>
   );
 }
